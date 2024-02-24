8ea455c9d1644eeef5fd3ab04edb73845ab425bc
orosmatthew <orosmatthew@pm.me>
2024-02-24 12:49:26 -0500

    Make window resizable

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,65 @@
-#include <algorithm>
-
 #include <raylib-cpp.hpp>
 
-#include "sizes.hpp"
+#define RAYGUI_IMPLEMENTATION
+#include <raygui.h>
+
+#include "res/roboto-regular.h"
 #include "wave_sim.hpp"
 #include "wave_sim_renderer.hpp"
 
 namespace rl = raylib;
 
-std::optional<Vector2i> mouse_to_sim(const Sizes& sizes, const rl::Vector2 mouse_pos)
+constexpr int sim_size = 1600;
+
+static int toolbar_height()
 {
-    if (!(mouse_pos.x >= 0 && mouse_pos.x < static_cast<float>(sizes.window_width()) && mouse_pos.y >= 0
-          && mouse_pos.y < static_cast<float>(sizes.window_height()))) {
-        return std::nullopt;
-    }
-    if (mouse_pos.y <= static_cast<float>(sizes.toolbar_height())) {
-        return std::nullopt;
-    }
-    const rl::Vector2 mouse_pos_sim = mouse_pos - rl::Vector2(0.0f, static_cast<float>(sizes.toolbar_height()));
+    constexpr float toolbar_ratio = 0.1f;
+    return std::max(static_cast<int>(static_cast<float>(GetScreenHeight()) * toolbar_ratio), 100);
+}
+
+static rl::Rectangle sim_screen_rect()
+{
+    const int size = std::max(std::min(GetScreenWidth(), GetScreenHeight() - toolbar_height()), 1);
+    return { static_cast<float>(GetScreenWidth()) / 2.0f - static_cast<float>(size) / 2.0f,
+             static_cast<float>(toolbar_height()),
+             static_cast<float>(size),
+             static_cast<float>(size) };
+}
+
+static std::optional<Vector2i> mouse_to_sim(const rl::Vector2 mouse_pos)
+{
     const rl::Vector2 sim_pos_f
-        = mouse_pos_sim * static_cast<float>(sizes.sim_size()) / static_cast<float>(sizes.window_width());
+        = (mouse_pos - sim_screen_rect().GetPosition()) * sim_size / sim_screen_rect().GetWidth();
     return Vector2i { static_cast<int>(sim_pos_f.x), static_cast<int>(sim_pos_f.y) };
 }
 
-void handle_scaling_inputs(rl::Window& window, Sizes& sizes)
+void handle_font_scale_inputs(rl::Font& font)
 {
+    std::optional<int> new_size;
     if (IsKeyPressed(KEY_EQUAL) && IsKeyDown(KEY_LEFT_CONTROL) && IsKeyDown(KEY_LEFT_SHIFT)) {
-        if (sizes.rescale(sizes.scale() + 0.1f)) {
-            window.SetSize(sizes.window_width(), sizes.window_height());
-        }
+        new_size = font.GetBaseSize() + 1;
     }
     if (IsKeyPressed(KEY_MINUS) && IsKeyDown(KEY_LEFT_CONTROL) && IsKeyDown(KEY_LEFT_SHIFT)) {
-        if (sizes.rescale(sizes.scale() - 0.1f)) {
-            window.SetSize(sizes.window_width(), sizes.window_height());
-        }
+        new_size = font.GetBaseSize() - 1;
+    }
+    if (new_size.has_value()) {
+        font = LoadFontFromMemory(
+            ".ttf",
+            font_robot_regular_ttf_bin,
+            static_cast<int>(font_robot_regular_ttf_bin_size),
+            new_size.value(),
+            nullptr,
+            0);
+        GuiSetFont(font);
+        GuiSetStyle(DEFAULT, TEXT_SIZE, new_size.value());
     }
 }
 
-void handle_sim_inputs(WaveSim& wave_sim, const Sizes& sizes)
+static void handle_sim_inputs(WaveSim& wave_sim)
 {
     const rl::Vector2 mouse_pos = GetMousePosition();
-    if (const std::optional<Vector2i> sim_pos = mouse_to_sim(sizes, mouse_pos); sim_pos.has_value()) {
+    if (const std::optional<Vector2i> sim_pos = mouse_to_sim(mouse_pos);
+        sim_pos.has_value() && wave_sim.in_bounds(sim_pos.value())) {
         if (!IsKeyDown(KEY_SPACE) && IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             wave_sim.add_at(sim_pos.value(), 10.0);
         }
@@ -71,35 +90,28 @@ void handle_sim_inputs(WaveSim& wave_sim, const Sizes& sizes)
     }
 }
 
-void draw(const WaveSimRenderer& sim_renderer, const Sizes& sizes)
+static void draw(const WaveSimRenderer& sim_renderer)
 {
     BeginDrawing();
-    ClearBackground(WHITE);
+    ClearBackground(LIGHTGRAY);
     DrawTexturePro(
-        sim_renderer.texture(),
-        { 0.0f, 0.0f, static_cast<float>(sizes.sim_size()), static_cast<float>(sizes.sim_size()) },
-        { 0.0f,
-          static_cast<float>(sizes.toolbar_height()),
-          static_cast<float>(sizes.window_width()),
-          static_cast<float>(sizes.window_width()) },
-        { 0.0f, 0.0f },
-        0.0f,
-        WHITE);
-    DrawFPS(10, sizes.toolbar_height() + 10);
+        sim_renderer.texture(), { 0.0f, 0.0f, sim_size, sim_size }, sim_screen_rect(), { 0.0f, 0.0f }, 0.0f, WHITE);
+    DrawFPS(10, toolbar_height() + 10);
+    GuiButton({ 10.0f, 10.0f, 100.0f, 40.0f }, "Hello World!");
     EndDrawing();
 }
 
 int main()
 {
-    Sizes sizes(1.0f, 1600);
-    rl::Window window { sizes.window_width(), sizes.window_height(), "Wave Simulation" };
-    if (rl::Vector2 dpi_scale = GetWindowScaleDPI(); sizes.scale() != dpi_scale.x) {
-        sizes.rescale(dpi_scale.x);
-    }
-    window.SetSize(sizes.window_width(), sizes.window_height());
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    rl::Window window { 600, 800, "Wave Simulation" };
+    rl::Font roboto_font = LoadFontFromMemory(
+        ".ttf", font_robot_regular_ttf_bin, static_cast<int>(font_robot_regular_ttf_bin_size), 18, nullptr, 0);
+    GuiSetFont(roboto_font);
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 18);
 
     auto sim_props = WaveSim::Properties {
-        .size = sizes.sim_size(),
+        .size = sim_size,
         .wave_speed = 0.5,
         .grid_spacing = 1.0,
         .timestep = 1.0,
@@ -114,13 +126,13 @@ int main()
     // SetTargetFPS(60.0f);
 
     while (!window.ShouldClose()) {
-        handle_scaling_inputs(window, sizes);
+        handle_font_scale_inputs(roboto_font);
 
-        handle_sim_inputs(wave_sim, sizes);
+        handle_sim_inputs(wave_sim);
         wave_sim.update();
         sim_renderer.update(wave_sim);
 
-        draw(sim_renderer, sizes);
+        draw(sim_renderer);
     }
 
     return EXIT_SUCCESS;
